5d231df5f2b968bbc1b3ce348eeb6f6a9b4a6f88
matsueushi <matsueushi@gmail.com>
2023-12-16 14:14:47 +0000

    auth

--- heuri/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ heuri/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "heuri8d4a17a68d4a17a6": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
