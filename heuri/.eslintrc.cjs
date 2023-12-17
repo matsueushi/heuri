24148a70ad6455ceac8751c9ac64c9633ab8e3c6
matsueushi <matsueushi@gmail.com>
2023-12-17 00:54:49 +0000

    Authを設定

--- heuri/.eslintrc.cjs
+++ heuri/.eslintrc.cjs
@@ -1,16 +1,19 @@
-/* eslint-env node */
-
 module.exports = {
+  root: true,
   env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
   ],
+  ignorePatterns: ["dist"],
   parser: "@typescript-eslint/parser",
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   plugins: ["react-refresh"],
   rules: {
     "react-refresh/only-export-components": "warn",
+    "semi": ["error", "always"],
+    "quotes": ["error", "double"],
+    "eol-last": ["error", "always"],
   },
 };
--- heuri/src/App.tsx
+++ heuri/src/App.tsx
@@ -1,3 +1,8 @@
+import { type AuthUser } from "aws-amplify/auth";
+import { type UseAuthenticator } from "@aws-amplify/ui-react-core";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+
 import { Refine } from "@refinedev/core";
 import routerBindings, { NavigateToResource, UnsavedChangesNotifier } from "@refinedev/react-router-v6";
 import dataProvider from "@refinedev/simple-rest";
@@ -8,7 +13,12 @@ import { Layout } from "./components/layout";
 
 import "./App.css";
 
-const App = () => {
+type AppProps = {
+  signOut?: UseAuthenticator["signOut"]; //() => void;
+  user?: AuthUser;
+};
+
+const App = ({ signOut, user }: AppProps) => {
   return (
     <BrowserRouter>
       <Refine
@@ -32,6 +42,10 @@ const App = () => {
           <Route
             element={
               <Layout>
+                Hello, user {user?.username}
+                <button onClick={signOut} >
+                  SignOut
+                </button>
                 <Outlet />
               </Layout>
             }
@@ -50,4 +64,12 @@ const App = () => {
     </BrowserRouter>
   );
 };
-export default App;
+
+// サインアップしない
+const withAuthenticatorOptions = {
+  hideSignUp: true,
+};
+
+const AppWithAuth = withAuthenticator(App, withAuthenticatorOptions);
+
+export default AppWithAuth;
