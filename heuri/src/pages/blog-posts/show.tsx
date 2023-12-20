ecafac88d2fe727aedf1fd33be486c497e7497e5
matsueushi <matsueushi@gmail.com>
2023-12-20 13:13:49 +0000

    生成されたコードで上書き

--- heuri/src/pages/blog-posts/show.tsx
+++ heuri/src/pages/blog-posts/show.tsx
@@ -1,5 +1,83 @@
-import { HeadlessInferencer } from "@refinedev/inferencer/headless";
+import React from "react";
+import {
+    useShow,
+    useResource,
+    useNavigation,
+    IResourceComponentsProps,
+    useOne,
+} from "@refinedev/core";
 
-export const ShowPost = () => {
-    return <HeadlessInferencer />;
+export const BlogPostShow: React.FC<IResourceComponentsProps> = () => {
+    const { edit, list } = useNavigation();
+    const { id } = useResource();
+    const { queryResult } = useShow();
+    const { data, isLoading } = queryResult;
+
+    const record = data?.data;
+
+    const { data: categoryData, isLoading: categoryIsLoading } = useOne({
+        resource: "categories",
+        id: record?.category?.id || "",
+        queryOptions: {
+            enabled: !!record,
+        },
+    });
+
+    return (
+        <div style={{ padding: "16px" }}>
+            <div
+                style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                }}
+            >
+                <h1>Blog Post Show</h1>
+                <div style={{ display: "flex", gap: "8px" }}>
+                    <button onClick={() => list("blog_posts")}>
+                        Blog Posts
+                    </button>
+                    <button onClick={() => edit("blog_posts", id ?? "")}>
+                        Edit
+                    </button>
+                </div>
+            </div>
+            <div>
+                <div style={{ marginTop: "6px" }}>
+                    <h5>Id</h5>
+                    <div>{record?.id ?? ""}</div>
+                </div>
+                <div style={{ marginTop: "6px" }}>
+                    <h5>Title</h5>
+                    <div>{record?.title}</div>
+                </div>
+                <div style={{ marginTop: "6px" }}>
+                    <h5>Content</h5>
+                    <p>{record?.content}</p>
+                </div>
+                <div style={{ marginTop: "6px" }}>
+                    <h5>Category</h5>
+                    <div>
+                        {categoryIsLoading ? (
+                            <>Loading...</>
+                        ) : (
+                            <>{categoryData?.data?.title}</>
+                        )}
+                    </div>
+                </div>
+                <div style={{ marginTop: "6px" }}>
+                    <h5>Status</h5>
+                    <div>{record?.status}</div>
+                </div>
+                <div style={{ marginTop: "6px" }}>
+                    <h5>Created At</h5>
+                    <div>
+                        {new Date(record?.createdAt).toLocaleString(undefined, {
+                            timeZone: "UTC",
+                        })}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
