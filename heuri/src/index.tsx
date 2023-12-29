75225c5aeb0e30883f9a1af9613b30f6e9d80533
matsueushi <matsueushi@gmail.com>
2023-12-29 14:10:50 +0000

    react-adminに変更

--- heuri/src/index.tsx
+++ heuri/src/index.tsx
@@ -1,13 +1,9 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './App';
 
-import App from "./App";
-
-const container = document.getElementById("root") as HTMLElement;
-const root = createRoot(container);
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
