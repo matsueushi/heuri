a69ce39d2d67e8b9c9e99dfe9f5cd5e7dd53d794
matsueushi <matsueushi@gmail.com>
2023-12-30 08:47:28 +0000

    cognito

--- heuri/src/App.tsx
+++ heuri/src/App.tsx
@@ -1,15 +1,26 @@
+import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from "react-admin";
+import { CognitoAuthProvider, Login } from "ra-auth-cognito";
+import { CognitoUserPool } from "amazon-cognito-identity-js";
+import { dataProvider } from "./dataProvider";
 
-import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-admin';
-import { dataProvider } from './dataProvider';
-import { authProvider } from './authProvider';
+import amplifyconfig from "./amplifyconfiguration.json";
+const userPool = new CognitoUserPool({
+    UserPoolId: amplifyconfig.aws_user_pools_id,
+    ClientId: amplifyconfig.aws_user_pools_web_client_id,
+});
+const authProvider = CognitoAuthProvider(userPool);
 
-export const App = () => (
-    <Admin
-        dataProvider={dataProvider}
-        authProvider={authProvider}
-    >
-        <Resource name="posts" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
-        <Resource name="comments" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
-    </Admin>
-);
+export const App = () => {
+    console.log(amplifyconfig);
+    return (
+        <Admin
+            dataProvider={dataProvider}
+            authProvider={authProvider}
+            loginPage={Login}
+        >
+            <Resource name="posts" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+            <Resource name="comments" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+        </Admin>
+    );
+};
 
