db1fb53d404c71b0f5283e53207708c348fd40d6
matsueushi <matsueushi@gmail.com>
2024-01-06 03:12:23 +0000

    layout

--- heuri/src/components/submissions/SubmissionCompare.tsx
+++ heuri/src/components/submissions/SubmissionCompare.tsx
@@ -1,14 +1,21 @@
-import { Show } from "react-admin";
+import { EditButton, Show, ShowButton, TopToolbar } from "react-admin";
 import { useParams } from "react-router-dom";
 import { SubmissionShowLayout } from "./SubmissionShowLayout";
+
+const SubmissionCompareActions = () => (
+    <TopToolbar>
+        <ShowButton />
+        <EditButton />
+    </TopToolbar>
+);
 
 export const SubmissionCompare = () => {
     const { submissionId } = useParams();
 
     return <>
-        <Show resource="submissions" id={submissionId}>
+        <Show resource="submissions" id={submissionId} actions={<SubmissionCompareActions />} >
             <SubmissionShowLayout />
-        </Show>
+        </Show >
         something...
     </>;
 };
--- heuri/src/components/submissions/SubmissionShow.tsx
+++ heuri/src/components/submissions/SubmissionShow.tsx
@@ -1,6 +1,7 @@
 import { Button, DateField, EditButton, Labeled, NumberField, ReferenceField, Show, SimpleShowLayout, TextField, TopToolbar, useRecordContext, useResourceContext } from "react-admin";
 import { TestCaseList } from "../testCases/TestCaseList";
 import CompareIcon from "@mui/icons-material/Compare";
+import { SubmissionShowLayout } from "./SubmissionShowLayout";
 
 const CompareButton = () => {
     const resource = useResourceContext();
@@ -19,22 +20,16 @@ const SubmissionShowActions = () => (
 );
 
 export const SubmissionShow = () => (
-    <Show actions={<SubmissionShowActions />}>
-        <SimpleShowLayout>
-            <TextField source="id" />
-            <ReferenceField source="contestId" reference="contests">
-                <TextField source="name" />
-            </ReferenceField>
-            <TextField source="functionName" />
-            <TextField source="description" />
-            <TextField source="status" />
-            <NumberField source="testcases" />
-            <NumberField source="completed" />
-            <DateField source="createdAt" showTime />
-            <DateField source="updatedAt" showTime />
-            <Labeled source="TestCases">
-                <TestCaseList />
-            </Labeled>
-        </SimpleShowLayout>
-    </Show>
+    <>
+        <Show actions={<SubmissionShowActions />}>
+            <SubmissionShowLayout />
+        </Show>
+        <Show actions={<></>}>
+            <SimpleShowLayout>
+                <Labeled source="TestCases">
+                    <TestCaseList />
+                </Labeled>
+            </SimpleShowLayout>
+        </Show >
+    </>
 );
